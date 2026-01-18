54e1ed648fa2c5181badfe98c73d483c41e15b4e
KOTIPALLI SRIKESH <125877167+srikesh2k4@users.noreply.github.com>
2026-01-18 18:51:27 +0530

    docs(readme) modified

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Loading } from './features/dashboard/pages/loading/loading';
+
+@Component({
+  selector: 'app-root',
+  // imports: [RouterOutlet,Loading],
+  imports: [RouterOutlet, Loading],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
